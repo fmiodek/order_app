ca7e590c7010680f2e9f843ef9f1123716cdde46
fmiodek <fmiodek@gmx.de>
2023-11-13 12:14:11 +0100

    display code cleanup

--- app/display/display.js
+++ app/display/display.js
@@ -4,25 +4,18 @@ const ulZubereitung = document.querySelector(".ul-zubereitung");
 const ulAbholung = document.querySelector(".ul-abholung");
 
 socket.on("message", data => {
-    
-    ulZubereitung.innerHTML = "";
-    ulAbholung.innerHTML = "";
-    
-
-    Object.keys(data[0]).forEach( (key) => {
-        let orderDiv = document.createElement("div");
-        orderDiv.classList.add("order-element");
-        let orderText = document.createElement("span");
-        orderText.innerHTML = key;
-        orderDiv.appendChild(orderText);
-        ulZubereitung.appendChild(orderDiv);
-    });
-    Object.keys(data[1]).forEach( (key) => {
-        let orderDiv = document.createElement("div");
-        orderDiv.classList.add("order-element");
-        let orderText = document.createElement("span");
-        orderText.innerHTML = key;
-        orderDiv.appendChild(orderText);
-        ulAbholung.appendChild(orderDiv);
-    });
+    updateUl(ulZubereitung, data[0]);
+    updateUl(ulAbholung, data[1]);
 });
+
+function updateUl (ul, data) {
+    ul.innerHTML = "";
+    Object.keys(data).forEach( (key) => {
+        let orderDiv = document.createElement("div");
+        orderDiv.classList.add("order-element");
+        let orderText = document.createElement("span");
+        orderText.innerHTML = key;
+        orderDiv.appendChild(orderText);
+        ul.appendChild(orderDiv);
+    });
+}
